aafd8f518ac7aaa90de023e0597e1abb176259fd
Connor Weng <foamtea30@gmail.com>
2014-07-31 14:49:06 +0800

    add console output

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,14 @@
 $(function () {
     $('.like').on('click', function () {
         $.post('/like/inc', function (data) {
+            console.log('inc:' + data);
             updateCount();
         });
     });
 
     var updateCount = function () {
         $.get('/like', function (data) {
+            console.log('update:' + data);
             $('#like-count').val(data);
         });
     };
