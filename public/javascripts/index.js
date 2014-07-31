cd885ab8e54fbace681c74d36b49b0614d28b4cd
Connor Weng <foamtea30@gmail.com>
2014-07-31 14:43:13 +0800

    introduce redis

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,15 @@
 $(function () {
     $('.like').on('click', function () {
         $.post('/like/inc', function (data) {
-
+            updateCount();
         });
     });
+
+    var updateCount = function () {
+        $.get('/like', function (data) {
+            $('#like-count').val(data);
+        });
+    };
+
+    updateCount();
 });
---
+++ routes/like.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var redis = require('redis');
+var client = redis.createClient(80, 'redis.duapp.com');
+
+client.on('error', function (err) {
+    console.log('Error:' + err);
+});
+
+client.auth('dOWdHb6VXq210YcFLwx5OLBU-Vl6eZEtSa1e9eGq1VOZDv17HQzU7yRB1-mwXJaYohdbMIWwJSAcHa');
+
+router.post('/inc', function(req, res) {
+    client.incr('like:count');
+});
+
+router.get('/', function(req, res) {
+    client.get('like:count', function (err, reply) {
+        res.send('count:' + reply.toString());
+    });
+});
+
+module.exports = router;
